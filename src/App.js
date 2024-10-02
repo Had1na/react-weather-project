2a821e6dc24631880e62db40bc0f576343387414
Had1na <gadubas@gmail.com>
2024-10-02 17:39:46 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Gabriella Dubas and is open-sourced on {""}
-        <a
-          href="https://github.com/Had1na/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Gabriella Dubas and is open-sourced on {""}
+          <a
+            href="https://github.com/Had1na/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
